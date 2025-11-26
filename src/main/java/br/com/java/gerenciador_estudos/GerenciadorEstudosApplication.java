83af9d558a118926e56270e044b3d2793c63aa73
angelokapunda <angelokapunda@gmail.com>
2025-11-26 09:02:43 -0300

    :tada: Primeiro commit do projeto

---
+++ src/main/java/br/com/java/gerenciador_estudos/GerenciadorEstudosApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.gerenciador_estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorEstudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/gerenciador_estudos/GerenciadorEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.gerenciador_estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
