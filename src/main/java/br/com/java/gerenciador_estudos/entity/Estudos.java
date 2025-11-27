d2a2df3f8039deb25ff87a4a09b3a7f93fed3070
angelokapunda <angelokapunda@gmail.com>
2025-11-26 23:17:04 -0300

    :sparkles: Contrução da classe de modelo

--- src/main/java/br/com/java/gerenciador_estudos/entity/Estudos.java
+++ src/main/java/br/com/java/gerenciador_estudos/entity/Estudos.java
@@ -1,7 +1,95 @@
 package br.com.java.gerenciador_estudos.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Estudos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+    private String assunto;
+    private String prioridade;
+    private String status;
+
+    @Column(name = "data_de_criacao")
+    @CreationTimestamp
+    private LocalDateTime dataDeCriacao;
+
+    @Column(name = "data_de_finalizacao")
+    private LocalDateTime dataDeFinalizacao;
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public LocalDateTime getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDateTime dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public LocalDateTime getDataDeFinalizacao() {
+        return dataDeFinalizacao;
+    }
+
+    public void setDataDeFinalizacao(LocalDateTime dataDeFinalizacao) {
+        this.dataDeFinalizacao = dataDeFinalizacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
